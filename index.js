c35d5f1262e48206fbe1be72e757625e3eecab01
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-04-24 16:24:36 +0200

    Add click event listeners to show and hide sidebar buttons

--- index.js
+++ index.js
@@ -171,8 +171,8 @@ function setupEventListeners() {
   });
 
   // Show sidebar event listener
-  elements.hideSideBarBtn.click() => toggleSidebar(false));
-  elements.showSideBarBtn.click() => toggleSidebar(true));
+  elements.hideSideBarBtn.addEventListener('click',() => toggleSidebar(false));
+  elements.showSideBarBtn.addEventListener('click',() => toggleSidebar(true));
 
   // Theme switch event listener
   elements.themeSwitch.addEventListener('change', toggleTheme);
