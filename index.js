df425c0ded8a288c560389b592efd1089dd3bd19
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-04-25 00:08:38 +0200

    use ' instead of "

--- index.js
+++ index.js
@@ -15,26 +15,27 @@ import { initialData } from "./initialData.js";
 
 // Function checks if local storage already has data, if not it loads initialData to localStorage
 function initializeData() {
-  if (!localStorage.getItem("tasks")) {
-    localStorage.setItem("tasks", JSON.stringify(initialData));
-    localStorage.setItem("showSideBar", "true");
+  if (!localStorage.getItem('tasks')) {
+    localStorage.setItem('tasks', JSON.stringify(initialData)); 
+    localStorage.setItem('showSideBar', 'true')
   } else {
-    console.log("Data already exists in localStorage");
+    console.log('Data already exists in localStorage');
   }
 }
 initializeData();
 
 // TASK: Get elements from the DOM
 const elements = {
-  headerBoardName: document.getElementById("header-board-name"),
-  columnDivs: document.querySelectorAll(".column-div"),
-  filterDiv: document.getElementById("filterDiv"),
-  hideSideBarBtn: document.getElementById("hide-side-bar-btn"),
-  showSideBarBtn: document.getElementById("show-side-bar-btn"),
-  themeSwitch: document.getElementById("switch"),
-  createNewTaskBtn: document.getElementById("add-new-task-btn"),
-  modalWindow: document.querySelector(".modal-window"),
-  editTaskModal: document.querySelector(".edit-task-modal-window"),
+  headerBoardName : document.getElementById('header-board-name'),
+  columnDivs: document.querySelectorAll('.column-div'),
+  filterDiv: document.getElementById('filterDiv'),
+  hideSideBarBtn: document.getElementById('hide-side-bar-btn'),
+  showSideBarBtn: document.getElementById('show-side-bar-btn'),
+  themeSwitch: document.getElementById('switch'),
+  createNewTaskBtn: document.getElementById('add-new-task-btn'),
+  modalWindow: document.querySelector('.modal-window'),
+  editTaskModal: document.querySelector('.edit-task-modal-window')
+  
 };
 
 let activeBoard = "";
