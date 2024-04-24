1bd489fa6a5f4d7e392a14994a80166e6b94ce3e
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-04-24 15:38:58 +0200

    Set active board to local storage board if it exists, or to the first board in the array

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function fetchAndDisplayBoardsAndTasks() {
   displayBoards(boards);
   if (boards.length > 0) {
     const localStorageBoard = JSON.parse(localStorage.getItem("activeBoard"))
-    activeBoard = localStorageBoard ? localStorageBoard ;  boards[0]; 
+    activeBoard = localStorageBoard ? localStorageBoard :  boards[0]; 
     elements.headerBoardName.textContent = activeBoard
     styleActiveBoard(activeBoard)
     refreshTasksUI();
