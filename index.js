f57003464af24b896d303c0c1236028bf4f5d74a
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-04-24 15:16:18 +0200

    Get filter div element

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function initializeData() {
 const elements = {
   headerBoardName : document.getElementById('header-board-name'),
   columnDivs: document.querySelectorAll('.column-div'),
+  filterDiv: document.getElementById('filterDiv'),
 }
 
 let activeBoard = ""
